aeb6ee7b32631d00f24181e99d42203204741d12
Iamneo <techadmin@iamneo.ai>
2023-09-11 07:19:18 +0000

    Backup Commit

--- test/src/App.js
+++ test/src/App.js
@@ -2,16 +2,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <>Hello </>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://iamneo.ai"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hello iamneo !!
+         iamneo !!
         </a>
       </header>
     </div>
